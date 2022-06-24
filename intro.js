cab281b85e9a44df44a7984947278a1b21d4338b
Mercy <mercyomole229@gmail.com>
2022-06-24 11:41:05 +0100

    First commit

---
+++ intro.js
@@ -0,0 +1,8 @@
+let myName = ("Mercy Irete Omole")
+let myHeight = ("1.86m")
+let myCountry = ("Nigeira")
+
+
+console.log("Name: " + myName)
+console.log("Height: " + myHeight)
+console.log("Country: " + myCountry)
